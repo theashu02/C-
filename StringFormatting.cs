8398b372eb1e5c2550fd66f0220b4e55c24895df
Ashutosh Chauhan <ashutoshchauhan1919@gmail.com>
2024-09-01 23:55:04 +0530

    string formatting

---
+++ StringFormatting.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string x = "ashu";
+        int age = 22;
+        string ans = string.Format("My name is{0}, and my age is {1}", x, age);
+        Console.WriteLine(ans);
+    }
+}
